596c66c7e08a1f403edc0c58f49f14127215dbb5
fajero123 <acostafediric2001@gmail.com>
2022-03-29 13:59:01 +0800

    added content

--- Activity1/src/app/app.module.ts
+++ Activity1/src/app/app.module.ts
@@ -1,20 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { FirstNavigationComponent } from './first-navigation/first-navigation.component';
 import { SecondNavigationComponent } from './second-navigation/second-navigation.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'first-navigation', pathMatch: 'full'},
+  { path: 'first-navigation', component: FirstNavigationComponent},
+  { path: 'second-navigation', component: SecondNavigationComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstNavigationComponent,
-    SecondNavigationComponent
+    SecondNavigationComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    [RouterModule.forRoot(routes)],
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class AppRoutingModule {}
